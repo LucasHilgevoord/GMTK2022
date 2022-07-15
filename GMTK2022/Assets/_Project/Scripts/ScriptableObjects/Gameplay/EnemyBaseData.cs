b1cf1a667a5d83ef1c230d26603397cdc681e002
Onur Sinan Togrul <o.sinantogrul@azerion.com>
2022-07-16 01:33:27 +0300

    Added enemy base data scriptable object
    In order to load the enemies from base data

---
+++ GMTK2022/Assets/_Project/Scripts/ScriptableObjects/Gameplay/EnemyBaseData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/// <summary>
+/// Scriptable object that holds the base hero data to only initially load enemies from
+/// </summary>
+[CreateAssetMenu(fileName = "EnemyBaseData", menuName = "ScriptableObjects/BaseData")]
+public class EnemyBaseData : ScriptableObject
+{
+    [SerializeField]
+    private int enemyID;
+
+    [SerializeField]
+    private string enemyName;
+
+    [SerializeField]
+    private float maxHealth;
+
+    public int GetEnemyID() => enemyID;
+    public string GetHeroName() => enemyName;
+    public float GetHealth() => maxHealth;
+}
+
+
